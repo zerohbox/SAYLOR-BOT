c499ad6dfa1ba4124bb1e5bce22f416d9f410657
n4mecit4-CB <n4mecit44@gmail.com>
2023-12-13 20:43:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,203 @@
+const { default : makeWASocket, DisconnectReason,useMultiFileAuthState,makeInMemoryStore,downloadContentFromMessage } = require('@whiskeysockets/baileys')
+const { Boom } = require('@hapi/boom')
+const P = require('pino')
+const cfonts = require('cfonts')
+const clc = require("cli-color");
+const numerodono = "+56961548695"
+const banner = cfonts.render("𝘠𝘖𝘔𝘐🥵 | 𝘉𝘖𝘛", {
+  font: 'pallet',
+  align: 'center',
+  gradient: ["green","blue"]
+})
+
+const color = (text, color) => { return !color ? clc.bold(text) : clc.bold(text) }
+
+const prefixo = "."
+function getGroupAdmins(participants) {
+admins = []
+for (let i of participants) {
+if(i.admin == 'admin') admins.push(i.id)
+if(i.admin == 'superadmin') admins.push(i.id)
+}
+return admins
+}
+
+async function connectToWhatsApp () {
+const store = makeInMemoryStore({ logger: P().child({ level: "silent", stream: "store" })})
+console.log(banner.string)
+console.log(`Bienvenido Yumi Bot\nEsta base fue creada por n4mecit4`)
+const { state, saveCreds } = await useMultiFileAuthState('./qr_code')
+
+    const sock = makeWASocket({
+        logger : P({ level : "silent" }),
+        auth : state,
+        browser: ["FireFox (linux)"],
+        printQRInTerminal: true
+    })
+    
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update
+        if(connection === 'close') {
+            const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
+            console.log('Error en la conexión ', lastDisconnect.error, ', Reconectando ', shouldReconnect)
+            // reconnect if not logged out
+            if(shouldReconnect) {
+                connectToWhatsApp()
+            }
+        } else if(connection === 'open') {
+            console.log('Conexion exitosa')
+        }
+    })
+      
+ sock.ev.on ('creds.update', saveCreds)   
+ 
+ store.bind(sock.ev)
+
+sock.ev.on('chats.set', () => {
+    console.log('Team chats', store.chats.all())
+})
+
+sock.ev.on('contacts.set', () => {
+    console.log('Team contactos', Object.values(store.contacts))
+})
+
+ sock.ev.on('messages.upsert', async m => {
+ try {
+ const info = m.messages[0]
+ if (!info.message) return 
+ if (info.key && info.key.remoteJid == "status@broadcast") return
+ const altpdf = Object.keys(info.message)
+ const type = altpdf[0] == "senderKeyDistributionMessage" ? altpdf[1] == "messageContextInfo" ? altpdf[2] : altpdf[1] : altpdf[0]
+const content = JSON.stringify(info.message)
+const from = info.key.remoteJid
+ var body = (type === 'conversation') ? info.message.conversation : (type == 'imageMessage') ? info.message.imageMessage.caption : (type == 'videoMessage') ? info.message.videoMessage.caption : (type == 'extendedTextMessage') ? info.message.extendedTextMessage.text : (type == 'buttonsResponseMessage') ? info.message.buttonsResponseMessage.selectedButtonId : (type == 'listResponseMessage') ? info.message.listResponseMessage.singleSelectReply.selectedRowId : (type == 'templateButtonReplyMessage') ? info.message.templateButtonReplyMessage.selectedId : ''
+
+const budy = (type === 'conversation') ? info.message.conversation : (type === 'extendedTextMessage') ? info.message.extendedTextMessage.text : ''
+
+var pes = (type === 'conversation' && info.message.conversation) ? info.message.conversation : (type == 'imageMessage') && info.message.imageMessage.caption ? info.message.imageMessage.caption : (type == 'videoMessage') && info.message.videoMessage.caption ? info.message.videoMessage.caption : (type == 'extendedTextMessage') && info.message.extendedTextMessage.text ? info.message.extendedTextMessage.text : ''
+
+// CONSTANTES IS  
+ const isGroup = info.key.remoteJid.endsWith('@g.us')
+const sender = isGroup ? info.key.participant : info.key.remoteJid
+const groupMetadata = isGroup ? await sock.groupMetadata(from) : ''
+const groupName = isGroup ? groupMetadata.subject : ''
+const groupDesc = isGroup ? groupMetadata.desc : ''
+const groupMembers = isGroup ? groupMetadata.participants : ''
+const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
+const nome = info.pushName ? info.pushName : ''
+const messagesC = pes.slice(0).trim().split(/ +/).shift().toLowerCase()
+const args = body.trim().split(/ +/).slice(1)
+const q = args.join(' ')
+const isCmd = body.startsWith(prefixo)
+const comando = isCmd ? body.slice(1).trim().split(/ +/).shift().toLocaleLowerCase() : null 
+const mentions = (teks, memberr, id) => {
+(id == null || id == undefined || id == false) ? sock.sendMessage(from, {text: teks.trim(), mentions: memberr}) : sock.sendMessage(from, {text: teks.trim(), mentions: memberr})}
+const quoted = info.quoted ? info.quoted : info
+const mime = (quoted.info || quoted).Mimetype || ""
+const sleep = async (ms) => {return new Promise(resolve => setTimeout(resolve, ms))}
+const pushname = info.pushName ? info.pushName : ''
+const isBot = info.key.fromMe ? true : false
+const isOwner = numerodono.includes(sender)
+const BotNumber = sock.user.id.split(':')[0]+'@s.whatsapp.net'
+const isGroupAdmins = groupAdmins.includes(sender) || false 
+const isBotGroupAdmins = groupAdmins.includes(BotNumber) || false
+const isUrl = (url) => { return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi')) }
+const deviceType = info.key.id.length > 21 ? 'Android' : info.key.id.substring(0, 2) == '3A' ? 'IPhone' : 'WhatsApp web'
+const options = { timeZone: 'America/Lima', hour12: false }
+const data = new Date().toLocaleDateString('PE', { ...options, day: '2-digit', month: '2-digit', year: '2-digit' })
+const hora = new Date().toLocaleTimeString('PE', options) 
+ 
+ // CONSTANTES NUEVAS
+ 
+ const enviar = (texto) => {
+ sock.sendMessage(from,{ text : texto }, {quoted : info})
+ }
+ 
+ // CONSTANTES IFF 
+ const isImage = type == "imageMessage"
+const isVideo = type == "videoMessage"
+const isAudio = type == "audioMessage"
+const isSticker = type == "stickerMessage"
+const isContact = type == "contactMessage"
+const isLocation = type == "locationMessage"
+const isProduct = type == "productMessage"
+const isMedia = (type === "imageMessage" || type === "videoMessage" || type === "audioMessage") 
+typeMessage = body.substr(0, 50).replace(/\n/g, "")
+if (isImage) typeMessage = "Image"
+else if (isVideo) typeMessage = "Video"
+else if (isAudio) typeMessage = "Audio"
+else if (isSticker) typeMessage = "Sticker"
+else if (isContact) typeMessage = "Contact"
+else if (isLocation) typeMessage = "Location"
+else if (isProduct) typeMessage = "Product"
+const isQuotedMsg = type === "extendedTextMessage" && content.includes("textMessage")
+const isQuotedImage = type === "extendedTextMessage" && content.includes("imageMessage")
+const isQuotedVideo = type === "extendedTextMessage" && content.includes("videoMessage")
+const isQuotedDocument = type === "extendedTextMessage" && content.includes("documentMessage")
+const isQuotedAudio = type === "extendedTextMessage" && content.includes("audioMessage")
+const isQuotedSticker = type === "extendedTextMessage" && content.includes("stickerMessage")
+const isQuotedContact = type === "extendedTextMessage" && content.includes("contactMessage")
+const isQuotedLocation = type === "extendedTextMessage" && content.includes("locationMessage")
+const isQuotedProduct = type === "extendedTextMessage" && content.includes("productMessage")
+
+const getFileBuffer = async (mediakey, MediaType) => {
+const stream = await downloadContentFromMessage(mediakey, MediaType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk]) }
+return buffer}
+ 
+ // RESPUESTAS AUTOMATICAS
+ const respuesta = {
+ espere : "Espere un momento porfavor",
+ dono : " este comando es solo usado por mi creador",
+ premiun: "compre la version premiun",
+ grupos : "este comando es solo para grupos",
+ privado : " 🕵‍♂️*Este comando solo se puede usar en el chat privado*",
+ error : " ⚠️ *Lo siento ocurrio un error, intentelo de nuevo Porfavor*",
+ textito : "😤 *Digita un texto Porfavor* ",
+ }
+ 
+// MENSAJES EN CONSOLA 
+ 
+ if (isGroup) {
+if (isGroup && isGroup) console.log(`${color('┏━━━━━━━━━┅┅┄┄⟞⟦ ⟝┄┄┉┉━━━━━━━━━┓', 'yellow')}\n${color('┃', 'yellow')} ${color('Número:', 'yellow')} ${color(sender.split('@')[0], 'white')}\n${color('┃', 'yellow')} ${color('Nombre:', 'yellow')} ${color(pushname, 'white')}\n${color('┃', 'yellow')} ${color('Horário:', 'yellow')} ${color(hora, 'white')}\n${color('┃', 'yellow')} ${color('comando:', 'yellow')} ${color(comando)}\n${color('┃', 'white')} ${color('Palabras:', 'yellow')} ${color(budy.length, 'yellow')}\n${color('┃', 'yellow')} ${color('Grupo:', 'yellow')} ${color(groupName, 'white')}\n${color('┗━━━━━━━━┅┅┄┄⟞⟦⟧⟝┄┄┉┉━━━━━━━━┛', 'yellow')}`)
+ if (!isGroup && isGroup) console.log(`${color('┏━━━━━━━━━┅┅┄┄⟞⟦ ⟝┄┄┉┉━━━━━━━━━┓', 'yellow')}\n${color('┃', 'yellow')} ${color('Número:', 'yellow')} ${color(sender.split('@')[0], 'white')}\n${color('┃', 'yellow')} ${color('Nombre:', 'yellow')} ${color(pushname, 'white')}\n${color('┃', 'yellow')} ${color('Horário:', 'yellow')} ${color(time, 'white')}\n${color('┃', 'yellow')} ${color('comando:', 'yellow')} ${color('No', 'white')}\n${color('┃', 'yellow')} ${color('Palabras:', 'yellow')} ${color(budy.length, 'white')}\n${color('┃', 'yellow')} ${color('Grupo:', 'yellow')} ${color(groupName, 'white')}\n${color('┗━━━━━━━━┅┅┄┄⟞⟦⟧⟝┄┄┉┉━━━━━━━━┛', 'yellow')}`)
+}
+ 
+switch(comando) {
+
+case "creador" :
+enviar("Yumi")
+break
+
+case "bot" :
+enviar("hola soy Yumi un bot a tu servicio")
+break
+
+
+
+
+// COMANDOS SIN PREFIJO
+default:
+
+} 
+ } catch (e) {
+ e = String(e)
+if (!e.includes("this.isZero") && !e.includes("Could not find MIME for Buffer <null>") && !e.includes("Cannot read property 'conversation' of null") && !e.includes("Cannot read property 'contextInfo' of undefined") && !e.includes("Cannot set property 'mtype' of undefined") && !e.includes("jid is not defined")) {
+console.log('Error : %s', color(e, 'yellow'))
+}
+ 
+ 
+ }       
+    })
+}
+// run in main file
+connectToWhatsApp()
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,16 @@
+Base creada por N4mecit4 Ofc.
+
+Está base se creo desde cero.
+
+
+Ya estas vinculado , sin estresarte.
+
+soy N4mecit4 .
+
+Bendiciones.
+
+Aquí te dejo número de contacto .
+
+Número : +56961548695
+
+Dios te bendiga !
